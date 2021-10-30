21bb9b80fd8a535d5329e25f4018f089ac2201fd
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-10-30 16:01:09 +0600

    added cors to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+const cors = require("cors");
+const router = require('./router.js')
 
 var PORT = 3000;
+app.use(cors());
 
 app.get('/', function(req, res) {
     res.status(200).send('Hello world');
