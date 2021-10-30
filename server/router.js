9deda279c6b434d22656ebc5b98066dde762371c
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-10-30 16:11:10 +0600

    added router for nft app

---
+++ server/router.js
@@ -0,0 +1,19 @@
+const Router = require('@koa/router');
+const router = new Router();
+const tokens = require('./tokens.json');
+
+router.get('/tokenId',async (ctx,next) =>{
+    const token =  tokens[ctx.params.tokenId];
+    if(typeof token === 'undefined'){
+        ctx.status =  400;
+        ctx.body ={
+            error : `tokendId ${ctx.params.tokenId} does not exist`
+        };
+        return;
+    }
+    ctx.body ={
+        tokenId: ctx.params.tokenId,
+        result : token
+    }
+});
+module.exports = router;
