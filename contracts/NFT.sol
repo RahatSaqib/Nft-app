7c8d77b742b4bd97f86c70f6f46c1a72adc14016
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-10-31 19:48:19 +0600

    added Nft for our app

---
+++ contracts/NFT.sol
@@ -0,0 +1,20 @@
+pragma solidity >= 0.5 < 0.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721{
+    uint public nextTokenId;
+    address public admin;
+
+    constructor() ERC721('NFT APP','NFT'){
+        admin = msg.sender;
+    }
+    function mint(address to) external{
+        require(msg.sender == admin ,'Only admin');
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+    function _baseUri() internal view override returns(string memory){
+        return 'https://secret-island-15010.herokuapp.com/';
+    }
+}
